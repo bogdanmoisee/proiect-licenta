3bb0f70f3add9937686804809ffa80200fc93c1e
Bogdan Moise <bogdan4280@gmail.com>
2023-03-29 22:54:21 +0300

    Add Login and Sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PersistLogin from './app/features/sessions/PersistLogin';
 import PrivateRoute from './app/features/routes/PrivateRoute';
 import Dashboard from './app/features/dashboard/Dashboard';
+import Logout from './app/features/sessions/Logout';
+import UpdateProfile from './app/features/sessions/UpdateProfile';
+import PublicOnlyRoute from './app/features/routes/PublicOnlyRoute';
+import Signup from './app/features/sessions/Signup';
+import Login from './app/features/sessions/Login';
 
 
 function App() {
@@ -26,6 +31,30 @@ function App() {
                   <Dashboard />
                 </PrivateRoute>
               } />
+
+              <Route path='/logout' element={
+                <PrivateRoute>
+                  <Logout />
+                </PrivateRoute>
+              } />
+              <Route path='/update-profile' element={
+                <PrivateRoute>
+                  <UpdateProfile />
+                </PrivateRoute>
+              } />
+
+              <Route path='/login' element={
+                <PublicOnlyRoute>
+                  <Login />
+                </PublicOnlyRoute>
+              } />
+
+              <Route path='/signup' element={
+                <PublicOnlyRoute>
+                  <Signup />
+                </PublicOnlyRoute>
+              } />
+              
             </Route>
           </Routes>
 
