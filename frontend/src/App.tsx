af2acbe90eec941f867b967af282bed2555e88c1
Bogdan Moise <bogdan4280@gmail.com>
2023-03-29 12:18:59 +0300

    Added routing and persistlogin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,56 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Counter } from './app/features/counter/Counter';
 import './App.css';
+import ResponsiveAppBar from './app/features/appbar/ResponsiveAppBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PersistLogin from './app/features/sessions/PersistLogin';
+import PrivateRoute from './app/features/routes/PrivateRoute';
+import Dashboard from './app/features/dashboard/Dashboard';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Router>
+
+        <header className="App-header">
+          <ResponsiveAppBar />
+        </header>
+        
+        <main>
+          <Routes>
+            <Route element={<PersistLogin />}>
+              <Route path="/" element ={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              } />
+            </Route>
+          </Routes>
+
+        </main>
+      </Router>
+
     </div>
   );
 }
